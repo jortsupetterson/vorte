03f72f626dbba364f924ed121d3b6c62817e7338
jortsupetterson <lehtinenjori03@gmail.com>
2025-11-23 21:50:26 +0200

    Calendar event form development at  /src/MainScripts/customElements/CalendarEventForm/*

---
+++ src/mainScripts/customElements/CalendarCategoryForm/Class.js
@@ -0,0 +1,2 @@
+export const CALENDAR_CATEGORY_FORM = "calendar-category-form";
+export class CalendarCategoryForm extends HTMLElement {}
--- src/mainScripts/customElements/CategoryManager/Class.js
+++
@@ -1,2 +0,0 @@
-export const CATEGORY_MANAGER = "category-manager";
-export class CategoryManager extends HTMLElement {}
